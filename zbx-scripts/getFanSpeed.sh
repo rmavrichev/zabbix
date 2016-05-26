4515430fd6b5c079acef06a23685fe6a5a681a6d
rmavrichev <roman.mavrichev@gmail.com>
2016-05-26 17:47:16 +0300

    Create getFanSpeed.sh

---
+++ zbx-scripts/getFanSpeed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Used in IBM IMM2 template, for fan speed monitoring.
+
+snmp_community=$1
+ip=$2
+fanindex=$3
+
+if [ $# -lt 3 ]
+ then
+     echo
+     echo " Not enough parameters"
+     echo " Usage: getFanSpeed.sh <SNMP_COMMUNITY> <IP> <FANINDEX>"
+  exit 2
+fi
+
+/usr/bin/snmpget -Ov -t 5 -c $snmp_community -v 1 $ip 1.3.6.1.4.1.2.3.51.3.1.3.2.1.3.$fanindex | /bin/sed "s/[^0-9]//g"
+
+exit 0
